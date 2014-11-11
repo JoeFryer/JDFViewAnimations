bce64eda03d790108356cca922c5eb28638745c5
Joe Fryer <joe.d.fryer@gmail.com>
2014-11-11 23:22:04 +0000

    Added UIView+JDFShakeAnimations.

---
+++ Pod/Classes/UIView+JDFShakeAnimations.h
@@ -0,0 +1,13 @@
+//
+//  UIView+JDFShakeAnimations.h
+//  JDFViewAnimations
+//
+//  Created by Joe Fryer on 11/11/2014.
+//  Copyright (c) 2014 Joe Fryer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (JDFShakeAnimations)
+
+@end
---
+++ Pod/Classes/UIView+JDFShakeAnimations.m
@@ -0,0 +1,13 @@
+//
+//  UIView+JDFShakeAnimations.m
+//  JDFViewAnimations
+//
+//  Created by Joe Fryer on 11/11/2014.
+//  Copyright (c) 2014 Joe Fryer. All rights reserved.
+//
+
+#import "UIView+JDFShakeAnimations.h"
+
+@implementation UIView (JDFShakeAnimations)
+
+@end
