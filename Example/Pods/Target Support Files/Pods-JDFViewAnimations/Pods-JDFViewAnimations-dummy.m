10c06d89ecd46fc1ce7733d5d1931fd61dca14bf
Joe Fryer <joe.d.fryer@gmail.com>
2014-11-11 23:12:54 +0000

    Pods.

---
+++ Example/Pods/Target Support Files/Pods-JDFViewAnimations/Pods-JDFViewAnimations-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_JDFViewAnimations : NSObject
+@end
+@implementation PodsDummy_Pods_JDFViewAnimations
+@end
---
+++ Example/Pods/Target Support Files/Pods-JDFViewAnimations/Pods-JDFViewAnimations-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// JDFViewAnimations
+#define COCOAPODS_POD_AVAILABLE_JDFViewAnimations
+#define COCOAPODS_VERSION_MAJOR_JDFViewAnimations 0
+#define COCOAPODS_VERSION_MINOR_JDFViewAnimations 1
+#define COCOAPODS_VERSION_PATCH_JDFViewAnimations 0
+
